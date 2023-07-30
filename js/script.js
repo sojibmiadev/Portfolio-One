39ca6bd23aaa1c942ccfd4668b142f23417ff137
Md. Sojib Mia <140948644+sojibmiadev@users.noreply.github.com>
2023-07-30 17:40:26 +0600

    Portfolio Project

---
+++ js/script.js
@@ -0,0 +1,37 @@
+
+// toggle navbar and responsive 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+
+showMenu('nav__toggle','nav-menu')
+
+
+
+// active and remove menu 
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+}    
